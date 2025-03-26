10bc30852b8e84e8dd6ca187b46b0f0778956b4e
Eric Harris <eahmedia22@gmail.com>
2025-03-26 13:55:52 -0400

    Cleaning up index.js and adding cors

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const path = require('path');
-const startgg = require('./server_modules/startgg');
-const challonge = require('./server_modules/challonge');
+require('dotenv').config()
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const startgg = require('./server_modules/startgg')
+const challonge = require('./server_modules/challonge')
+const port = process.env.PORT || 3015
 
-// parse application/json
-app.use(bodyParser.json({limit: '100mb'}));
+app.use(cors())
+app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.options('*', cors())
 app.post('/obf', async (req, res) => {
-  console.log(req.body.bracket.search('start.gg') != -1);
-  let obf;
+  console.log(req.body.bracket.search('start.gg') != -1)
+  let obf
   if (req.body.bracket.search('start.gg') != -1){
     try {
-      obf = await startgg.startGGBracket(req.body.bracket.trim());
+      obf = await startgg.startGGBracket(req.body.bracket.trim())
     }
     catch(e) {
       obf = {
@@ -40,13 +39,9 @@ app.post('/obf', async (req, res) => {
       error : "UNSUPPORTED SITE"
     }
   }
-  res.json(obf);
+  res.json(obf)
 })
 
-app.listen(process.env.PORT, () => {
-  console.log("LISTENING ON PORT:", process.env.port);
-});
-
-//startgg.startGGBracket("tournament/new-england-smash-invitational/event/ultimate-singles");
-
-//startgg.startGGBracket("https://www.start.gg/tournament/get-on-my-level-2023-canadian-fighting-game-championships/event/super-smash-bros-melee-singles");
+app.listen(port, () => {
+  console.log("LISTENING ON PORT:", port)
+})
